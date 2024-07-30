011d06250bfdaf0f0cf1e585f5fd58e6c6c2e6c6
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-07-30 14:49:42 +0530

    models and controllers edited

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import dbConnection from './config/db.js';
+import router from './routes/userRoute.js'
 
 dotenv.config();
 dbConnection()  //db connection config
@@ -9,6 +10,8 @@ const app=express()
 
 app.use(express.json())
 app.use(morgan('dev'))
+
+app.use('/api/user', router)
 
 app.get('/',(req,res)=>{
     res.send("<h1>Welcome to E-commerce Home Decor</h1>")
