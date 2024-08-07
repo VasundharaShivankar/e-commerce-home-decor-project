1c133f6c9a4d12bd4f02d1dbc4db0c86444d3c90
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-08-08 00:54:50 +0530

    middle ware updated for signin requirement

---
+++ middlewares/middlewares.js
@@ -0,0 +1,19 @@
+import JWT from "jsonwebtoken";
+import User from "../server/models/User.js";
+
+//Protected Routes 
+export const requireSignIn = async (req, res, next) => {
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//admin acceess
+
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,14 @@
 import express from 'express'
-import {registerController, loginController} from '../server/controllers/userController/userController.js';
+import {registerController, 
+    loginController,
+    testController
+} from '../server/controllers/userController/userController.js';
+
+import { isAdmin, requireSignIn } from "../middlewares/middlewares.js";
 
 const userRoutes=express.Router()
 
 userRoutes.post('/register', registerController);
 userRoutes.post('/login',loginController);
+router.get("/test", requireSignIn, isAdmin, testController);
 export default userRoutes;
